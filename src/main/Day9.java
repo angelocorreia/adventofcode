8d804d49149c657de71351c8b3773ea6bf4d9d75
angelo <angelo.1986@gmail.com>
2023-12-09 12:05:58 -0500

    day9

--- src/main/Day9.java
+++ src/main/Day9.java
@@ -9,39 +9,55 @@ import java.util.stream.Collectors;
 
 public class Day9 {
     public static void main(String[] args) throws Exception {
-        List<int[]> sequences = new ArrayList<>();
         BufferedReader reader = new BufferedReader(new FileReader(new File("./src/main/resources/day9.txt")));
         String s = reader.readLine();
         List<List<Integer>> sequenceList = new ArrayList<>();
+        List<List<Integer>> subLists = new ArrayList<>();
         while (s != null) {
-            sequences.add(Arrays.stream(s.split(" ")).mapToInt(Integer::valueOf).toArray());
             sequenceList.add(Arrays.asList(s.split(" ")).stream().map(Integer::valueOf).collect(Collectors.toList()));
             s = reader.readLine();
         }
-        int totalSum = 0;
-        for (int[] seq : sequences) {
-            totalSum = totalSum + getNextInSequence(seq, seq.length - 1, 0);
 
-        }
-        System.out.println("Total "+ totalSum);
+        int part1total = 0;
+        int part2total = 0;
+        int counter = 0;
+        for (List<Integer> seq : sequenceList) {
+            subLists.add(seq);
+            List<Integer> newList = getNextList(sequenceList.get(counter));
+            while (!newList.stream().allMatch(i -> i == 0)) {
+                subLists.add(newList);
+                newList = getNextList(newList);
+            }
 
-        for (int i=0; i<sequences.size();i++){
-            System.out.println(sequences.get(i));
+            for (int i = 0; i < subLists.size(); i++) {
+
+                part1total = part1total + subLists.get(i).get(subLists.get(i).size() - 1);
+
+            }
+            for (int i = subLists.size() - 2; i >= 0; i--) {
+                int toAdd = subLists.get(i).get(0) - subLists.get(i + 1).get(0);
+                subLists.get(i).add(0, toAdd);
+                if (i == 0) {
+                    part2total = part2total + toAdd;
+                }
+
+            }
+            counter++;
+            subLists.clear();
         }
+        System.out.println("Other total " + part1total);
+        System.out.println("Part 2 " + part2total);
+
     }
 
     public static List<Integer> getNextList(List<Integer> list) {
-        List<Integer> newList= new ArrayList<>();
-        for (int i =0;i<list.size()-2;i++){
-            newList.add(list.get(i+1)-list.get(i));
+        List<Integer> newList = new ArrayList<>();
+        for (int i = 0; i < list.size() - 1; i++) {
+            newList.add(list.get(i + 1) - list.get(i));
         }
         return newList;
     }
 
-    public static int getNextInSequence(int[] sequence, int index, int level) {
-        int lastInSequence = getFromSequence(sequence, index, level);
-        return lastInSequence + getSumOfDiff(sequence, index, level);
-    }
 
     public static int getFromSequence(int[] sequence, int index, int level) {
         if (level == 0) {
@@ -51,17 +67,6 @@ public class Day9 {
             int priorLower = getFromSequence(sequence, index, level - 1);
             return priorUpper - priorLower;
         }
-    }
-
-    public static int getSumOfDiff(int[] sequence, int index, int level) {
-        int lastInSequence = getFromSequence(sequence, index, level);
-        int nextLower = getFromSequence(sequence, index - 1, level);
-        int diff = lastInSequence - nextLower;
-        int sum = diff;
-        if (diff > 0) {
-            sum = sum + getSumOfDiff(sequence, index - 1, level + 1);
-        }
-        return sum;
     }
 
 
